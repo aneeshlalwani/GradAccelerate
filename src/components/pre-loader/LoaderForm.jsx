3a98b3be7088064b0d0e19169b06c5440e447ba3
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-10-06 11:53:44 +0500

    feat: add animation in to preloader for smooth transition

--- src/components/pre-loader/LoaderForm.jsx
+++ src/components/pre-loader/LoaderForm.jsx
@@ -1,37 +1,50 @@
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
+// eslint-disable-next-line react/prop-types
 const LoaderForm = ({ onSubmit }) => {
   const [username, setUsername] = useState("");
-
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (username) {
       onSubmit(username);
+      setIsFormSubmitted(true);
     }
   };
   return (
     <>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col justify-center items-center min-h-screen bg-gray-900 p-6 rounded-lg shadow-lg"
-      >
-        <label className="text-5xl text-center mb-10 font-extrabold text-emerald-400">
-          Hey there! 👋🏻 What&apos;s Your Good Name?
-        </label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="p-4 w-[30%] h-16 rounded-full mb-4 border bg-[#1c1c1c] text-white text-2xl focus:outline-none focus:ring-0 focus:border-gray-400 transition-all"
-          placeholder="Enter your name, please"
-        />
-        <button
-          type="submit"
-          className="bg-white text-xl text-black font-bold px-6 py-3 rounded-full hover:bg-emerald-700 hover:text-white transition-colors duration-300"
-        >
-          Submit
-        </button>
-      </form>
+      <div className="flex flex-col justify-center items-center min-h-screen bg-gray-900">
+        <AnimatePresence>
+          {!isFormSubmitted && (
+            <motion.form
+              onSubmit={handleSubmit}
+              className="flex flex-col justify-center items-center min-h-screen bg-gray-900 p-6 rounded-lg shadow-lg"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1.5 }}
+            >
+              <label className="text-5xl text-center mb-10 font-extrabold text-emerald-400">
+                Hey there! 👋🏻 What&apos;s Your Good Name?
+              </label>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="p-4 w-[40%] h-16 rounded-full mb-4 border bg-[#1c1c1c] text-white text-2xl focus:outline-none focus:ring-0 focus:border-gray-400 transition-all"
+                placeholder="Enter your name, please"
+              />
+              <button
+                type="submit"
+                className="bg-white text-xl text-black font-bold px-6 py-3 rounded-full hover:bg-emerald-700 hover:text-white transition-colors duration-300"
+              >
+                Submit
+              </button>
+            </motion.form>
+          )}
+        </AnimatePresence>
+      </div>
     </>
   );
 };
--- src/components/pre-loader/Preloader.jsx
+++ src/components/pre-loader/Preloader.jsx
@@ -1,19 +1,67 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+// eslint-disable-next-line react/prop-types
 const Preloader = ({ username }) => {
+  const [showText, setShowText] = useState(true);
+
+  // Broom Broom text will disappear after a 3000ms delay
+  setTimeout(() => setShowText(false), 3000);
+
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-[#1c1c1c] text-white">
-        <h1 className="text-4xl font-extrabold capitalize mb-6">
-          Yay, It&apos;s You, {username}! 😅
-        </h1>
-        <h2 className="text-7xl mb-4">Broom Broom! 🚗💨</h2>
-        <div className="w-[80%] text-center">
+      <motion.div
+        className="flex flex-col items-center justify-center min-h-screen bg-[#1c1c1c] text-white overflow-hidden"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        <motion.h1
+          className="text-4xl font-extrabold capitalize mb-6"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          Yay, It&apos;s You, {username}...! 😅
+        </motion.h1>
+        <AnimatePresence>
+          {showText && (
+            <motion.h2
+              className="text-6xl mb-4"
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: [1, 1.5, 1], opacity: 1 }}
+              transition={{
+                delay: 0.5,
+                duration: 0.7,
+                ease: "easeInOut",
+                repeat: 2,
+                repeatType: "loop",
+              }}
+              exit={{
+                x: "-100vw", // It Will Disappear Like a Car Speed.
+                opacity: 0,
+                transition: { duration: 0.4, ease: "easeIn" },
+              }}
+            >
+              {" "}
+              🚗💨 Broom Broom!{" "}
+            </motion.h2>
+          )}
+        </AnimatePresence>
+
+        <motion.div
+          className="w-[80%] text-center"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1.8, duration: 0.5 }}
+        >
           <h1 className="text-4xl font-bold capitalize mb-4">Hold tight!</h1>
           <h2 className="text-3xl font-bold mb-4">
             We&apos;re about to take you on a career-transforming journey that
             will unlock your potential!
           </h2>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </>
   );
 };
