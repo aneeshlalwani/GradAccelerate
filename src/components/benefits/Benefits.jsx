d6ae2a957f62d9b836f47868bc08279c746e2131
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-10-09 09:53:34 +0500

    fix(learning-tracks): update the animation for better user experience

--- src/components/benefits/Benefits.jsx
+++ src/components/benefits/Benefits.jsx
@@ -17,75 +17,37 @@ const Benefits = () => {
         >
           Improved Student Experience
         </motion.h1>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 items-center">
+        <motion.div
+          ref={ref}
+          initial={{ opacity: 0, x: -100 }}
+          animate={isInView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}
+          className="grid grid-cols-1 lg:grid-cols-3 gap-6 items-center"
+        >
           {/* Left Side Benefits Description */}
-          <motion.div
-            ref={ref}
-            initial={{ opacity: 0, y: 100 }}
-            animate={isInView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}
-            className="space-y-8"
-          >
-            {benefits.slice(0, 2).map((item, index) => (
-              <motion.div
-                key={item.id}
-                ref={ref}
-                initial={{ opacity: 0, x: 100 }}
-                animate={isInView ? { opacity: 1, x: 0 } : {}}
-                transition={{
-                  duration: 1.2,
-                  ease: "easeInOut",
-                  delay: 0.5 + index * 0.2,
-                }}
-                className="text-[#e5e0df] mb-16"
-              >
+          <div className="space-y-8">
+            {benefits.slice(0, 2).map((item) => (
+              <div key={item.id} className="text-[#e5e0df] mb-16">
                 <h2 className="text-3xl px-3 border border-lg rounded-sm inline">
                   {item.id}
                 </h2>
                 <h3 className="text-2xl font-bold my-3">{item.title}</h3>
                 <p className="text-lg font-semibold">{item.description}</p>
-              </motion.div>
+              </div>
             ))}
-          </motion.div>
+          </div>
           {/* Center Image of Boy*/}
-          <motion.div
-            ref={ref}
-            initial={{ opacity: 0 }}
-            animate={isInView ? { opacity: 1 } : {}}
-            transition={{
-              duration: 1.2,
-              ease: "easeInOut",
-              delay: 0.8,
-            }}
-            className="flex justify-center"
-          >
+          <div className="flex justify-center">
             <img
               src={boy}
               alt="students image in the center"
               className="w-full max-w-[300px] h-auto object-cover rounded-xl"
             />
-          </motion.div>
+          </div>
           {/* Right side descriptions */}
-          <motion.div
-            ref={ref}
-            initial={{ opacity: 0, y: 100 }}
-            animate={isInView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}
-            className="space-y-8"
-          >
-            {benefits.slice(2, 4).map((item, index) => (
-              <motion.div
-                key={item.id}
-                ref={ref}
-                initial={{ opacity: 0, x: -100 }}
-                animate={isInView ? { opacity: 1, x: 0 } : {}}
-                transition={{
-                  duration: 1.2,
-                  ease: "easeInOut",
-                  delay: 0.5 + index * 0.2,
-                }}
-                className="text-[#e5e0df] mb-16"
-              >
+          <div className="space-y-8">
+            {benefits.slice(2, 4).map((item) => (
+              <motion.div key={item.id} className="text-[#e5e0df] mb-16">
                 <h2 className="text-3xl px-3 border border-lg rounded-sm inline">
                   {item.id}
                 </h2>
@@ -93,8 +55,8 @@ const Benefits = () => {
                 <p className="text-lg font-semibold">{item.description}</p>
               </motion.div>
             ))}
-          </motion.div>
-        </div>
+          </div>
+        </motion.div>
       </section>
     </>
   );
